56ec5bee5b6fb2f6ed59e8e86907f863fd9a514f
Jonathon Rossi <jono@jonorossi.com>
2008-12-17 12:41:25 +0000

    Worked on NConsole.

--- src/NConsole/Properties/AssemblyInfo.cs
+++ src/NConsole/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 
-[assembly: InternalsVisibleTo("NConsole.Tests")]
+[assembly: InternalsVisibleTo("NConsole.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b79dbca337d01ce1a74f2630858e68fd2ccdff33dc3c71c695ec1a66119b315af2a789b7c50c100275e387e6fda2d2873933f581ded3cb3ed960516dd7932309eac27a76f280238b7a9f99fe63bc0cbd9ce1227afe789206623f06759467b040961f00e9b36d09f5d9ab701e27d42504c726ee78e5d197bfb58ee45b568fdfe2")]
